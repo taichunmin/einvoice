623f5c1aefa11f6daf9712047fa8a2e53c241ff8
戴均民 <taichunmin@gmail.com>
2017-03-29 15:59:33 +0800

    Code39

---
+++ chosen/vue-chosen.js
@@ -0,0 +1,97 @@
+/* global Vue, $ */
+Vue.component('select-chosen', {
+  template:
+    '<select @input="emitValue" :data-placeholder="placeholder" :class="class1 || {}" :multiple="multiple">' +
+    '<option v-for="(name, id) in normalOptions" :value="id">{{name}}</option>' +
+    '<optgroup :label="label" v-for="(groupOptions, label) in optgroups">' +
+    '<option v-for="(name, id) in groupOptions" :value="id">{{name}}</option>' +
+    '</optgroup>' +
+    '</select>',
+  props: {
+    value: {required: true},
+    options: {type: Object, required: true},
+    placeholder: {type: String, default: '選擇一個選項...'},
+    class1: {type: Object, default: function () { return {} }},
+    debug: {type: Boolean, default: false},
+    multiple: {type: Boolean, default: false}
+  },
+  mounted () {
+    $(this.$el).change(this.emitValue)
+    this.init()
+  },
+  computed: {
+    optgroups () {
+      let result = {}
+      let key
+      for (key in this.options) {
+        if (this.isObjectLike(this.options[key])) {
+          result[key] = this.options[key]
+        }
+      }
+      return result
+    },
+    normalOptions () {
+      let result = {}
+      let key
+      for (key in this.options) {
+        if (this.isNotObject(this.options[key])) {
+          result[key] = this.options[key]
+        }
+      }
+      return result
+    }
+  },
+  methods: {
+    init () {
+      if (this.debug) console.log('chosen:init')
+      $(this.$el).chosen({
+        disable_search_threshold: 5,
+        inherit_select_classes: true,
+        no_results_text: '什麼都沒找到...',
+        search_contains: true,
+        width: '100%'
+      })
+      this.$nextTick(function () {
+        // 修正 select 在 value 為空時 inserted 後會自動選擇第一個的問題
+        if (this.debug) console.log('reset', this.value)
+        $(this.$el).val(this.value).trigger('chosen:updated')
+      })
+      return this
+    },
+    destroy () {
+      if (this.debug) console.log('chosen:destroy')
+      $(this.$el).chosen('destroy')
+      return this
+    },
+    emitValue () {
+      let value = $(this.$el).val()
+      if (this.debug) console.log('emit', value)
+      this.$emit('input', value, 1)
+    },
+    isObjectLike (value) {
+      return value != null && typeof value === 'object'
+    },
+    isNotObject (value) {
+      return value == null || typeof value !== 'object'
+    }
+  },
+  watch: {
+    value () {
+      this.$nextTick(function () {
+        if (this.debug) console.log('chosen:updated', this.value)
+        $(this.$el).val(this.value).trigger('chosen:updated')
+      })
+    },
+    options () {
+      this.$nextTick(function () {
+        if (this.debug) console.log('chosen:rebuild', this.options.length)
+        this.destroy().init()
+      })
+    }
+  },
+  beforeDestroy () {
+    if (this.debug) console.log('chosen:beforeDestroy')
+    this.destroy()
+    $(this.$el).unbind('change', this.emitValue)
+  }
+})
---
+++ readme.md
@@ -0,0 +1,45 @@
+# 把電子發票手機條碼塞到 passbook 裡！
+
+![](https://i.imgur.com/gKX5mRR.png)
+
+1. 照著教學產生 Template
+1. Icon 和 Logo 可以用裁剪過的 [電子發票圖示](logo.png)
+1. Strip 使用底下產生網頁所產生的條碼
+
+## passdock
+
+* 網站: <http://passdock.com>
+* 教學
+  * <http://skychy.blogspot.tw/2012/12/passbook.html>
+  * <https://www.mobile01.com/topicdetail.php?f=383&t=5105768>
+* 底下的產生網頁已預設使用 passdock 所需的 620x250 的圖片
+
+## Pass2U
+
+* 網站: <https://www.pass2u.net/>
+* 教學
+  * <https://www.mobile01.com/topicdetail.php?f=383&t=5106555>
+
+## passkit
+
+* 網站: <https://create.passkit.com/>
+* 教學
+  * <https://medium.com/@Haraguroicha/%E5%A6%82%E4%BD%95%E7%94%A8-passkit-%E5%BB%BA%E7%AB%8B%E9%9B%BB%E5%AD%90%E7%99%BC%E7%A5%A8%E6%89%8B%E6%A9%9F%E6%A2%9D%E7%A2%BC-5522630b60d4>
+
+## 共通性載具條碼產生網頁
+
+![](https://imgur.com/VPJL0ye.png)
+
+<http://taichunmin.idv.tw/einvoice/>
+
+請用最新版 Chrome 瀏覽器
+
+## LICENSE
+
+[MIT](LICENSE)
+
+## 特別感謝
+
+1. BarcodesInc 提供的 [免費 Code 39 TTF 字型](https://www.barcodesinc.com/free-barcode-font/)
+2. FONT SQUIRREL [將 TTF 轉成各種網頁格式](https://www.fontsquirrel.com/tools/webfont-generator)
+4. [GitHub Pages 免費靜態網站](https://pages.github.com/)
